7a30766faee3b0af43559944384eaa84406f3b58
CAD Client <CAD Client>
2016-06-04 10:19:07 +0900

    Files added !

---
+++ ex40/ex40.py
@@ -0,0 +1,30 @@
+mystuff = {'apple': "I AM APPLES!"}
+print mystuff['apple']
+
+
+import mystuff
+
+mystuff.apple()
+print(mystuff.tangerine)
+
+
+class MyStuff(object):
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(selfself):
+        print("I AM CLASSY APPELS!")
+
+
+
+# end class MyStuff
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
+
+class Song(object):
+    def __InIt__(self, lyrics):
+        self.lyrics = lyrics
+
